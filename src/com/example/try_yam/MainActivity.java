45609ab2661aa51e3242be9e3684c880efefa068
gbz3 <gebizo3@gmail.com>
2013-09-16 12:28:12 +0900

    restore center and zoom

--- src/com/example/try_yam/MainActivity.java
+++ src/com/example/try_yam/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.yam;
+package com.example.try_yam;
 
 import jp.co.yahoo.android.maps.GeoPoint;
 import jp.co.yahoo.android.maps.MapActivity;
@@ -10,6 +10,7 @@ public class MainActivity extends MapActivity {
 
 	private static final String LAT_OF_CENTER = "LATITUDE_OF_CENTER";	// 緯度
 	private static final String LON_OF_CENTER = "LONGITUDE_OF_CENTER";	// 軽度
+	private static final String ZOOM_LEVEL = "ZOOM_LEVEL";	// ズームレベル
 
 	private MapView _mv;
 
@@ -17,7 +18,7 @@ public class MainActivity extends MapActivity {
 	public void onCreate( Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
 
-		_mv = new MapView( this, "dj0zaiZpPU12WHdRV3RESkZUNCZzPWNvbnN1bWVyc2VjcmV0Jng9Zjg-" );
+		_mv = new MapView( this, getString( R.string.appid ) );
 		_mv.setScalebar( true );
 
 		FrameLayout fl = new FrameLayout( this );
@@ -34,6 +35,7 @@ public class MainActivity extends MapActivity {
 	@Override
 	protected void onSaveInstanceState( Bundle outState ) {
 		if( _mv != null ) {
+			outState.putInt( ZOOM_LEVEL, _mv.getZoomLevel() );
 			outState.putInt( LAT_OF_CENTER, _mv.getMapCenter().getLatitudeE6() );
 			outState.putInt( LON_OF_CENTER, _mv.getMapCenter().getLongitudeE6() );
 		}
@@ -42,6 +44,7 @@ public class MainActivity extends MapActivity {
 	@Override
 	protected void onRestoreInstanceState( Bundle inState ) {
 		if( _mv != null && inState.containsKey( LAT_OF_CENTER ) && inState.containsKey( LON_OF_CENTER ) ) {
+			_mv.getMapController().setZoom( inState.getInt( ZOOM_LEVEL ) );
 			_mv.getMapController().setCenter(
 					new GeoPoint( inState.getInt( LAT_OF_CENTER ), inState.getInt( LON_OF_CENTER ) ) );
 		}
