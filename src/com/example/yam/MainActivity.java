92f83f0e02cfac2538b08134ca4ea9cd0f2b657f
gbz3 <gebizo3@gmail.com>
2013-09-16 11:02:47 +0900

    yam

---
+++ src/com/example/yam/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.yam;
+
+import jp.co.yahoo.android.maps.MapActivity;
+import android.os.Bundle;
+
+public class MainActivity extends MapActivity {
+
+	@Override
+	public void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+	}
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
